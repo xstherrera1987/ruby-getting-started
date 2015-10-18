d07a87b519c567b52ed8624e2cfc9e2711c875fd
Joe Herrera <xstherrera1987@gmail.com>
2015-10-18 02:32:37 +0000

    serve through 0.0.0.0 by default

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# default development server port
+# serve development app at http://0.0.0.0:3000 to forward through to host OS
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3000)
+    end
+  end
+end
+
